b3dda1ae93c8a1285d02f13a254089341fa0f394
Jaffer Ali <71866612+legendof17@users.noreply.github.com>
2021-03-25 19:43:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+model = Pipeline([('tfidf',TfidfVectorizer()),('model',SVC())])
 model = joblib.load('Email_Class')
 st.title('Spam Ham Classifier')
 ip = st.text_input('Enter your message')
